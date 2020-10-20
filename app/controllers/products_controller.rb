b4fdb9245c1a9050c968acd303f5a6904091e8bd
dErmAnHub <dtools001@gmail.com>
2020-10-20 21:36:11 +0900

    在庫登録機能
    railsのバージョンが5.0.7.2でform_withが使えなかった為、
    form_forで代用。機能の動きは確認できたので、次のブランチでバージョンアップした後に、form_withに変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,22 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to new_product_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :quantity, :low_quantity)
+  end
 end
